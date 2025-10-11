1b9da104aeb3f8956aaeed3849ec5cf41f73c0e6
SET264575 <100035024+SET264575@users.noreply.github.com>
2025-10-11 12:37:04 -0500

    Update records11.js

--- Work284/records11.js
+++ Work284/records11.js
@@ -91,6 +91,7 @@ function createRecord(i,j){
   x.setAttribute("class","button2");
   x.setAttribute("name",i);
   x.setAttribute("id","Btn"+String(i));
+  x.setAttribute("width","20");
  // x.setAttribute("width","70");
   x.setAttribute("onclick","deleteRoom(this.name)");
   tree2.appendChild(x);
@@ -356,6 +357,7 @@ function currentTime(){
 	var x = Date().toLocaleString();
 	return(x.slice(16,21));
 }
+
 
 
 
